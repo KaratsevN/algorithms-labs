5bf63dbd801e16a94bee98626da7656dc103c824
Nikita Karatsev <nekit-gm@live.com>
2019-04-10 09:58:39 +0600

    add lab2

--- lab2/DoubleLinkedList.py
+++ lab2/DoubleLinkedList.py
@@ -72,64 +72,111 @@ class DoubleLinkedList():
                 count += 1
             return count
 
-    def fill_deque(self):
+    def fill_deque(self, deque):
         string = raw_input("Введите строку: ")
         for i in range(0, len(string)):
             self.add(string[i])
+            deque.add(string[i])
 
-    def sort1(self):
+    def sort1(self, deque):
         current_node = self.head
+        curr = deque.tail
         while current_node is not None:
-            if current_node.next is None:
-                print(current_node.data, end=" <-> ")
+            deque.add(current_node.data)
+            current_node = current_node.next
+        #while current_node.next is not None:
+        #    current_node = current_node.next
+        curr = deque.head
+        while curr is not None:
+            if curr.next is None:
+                print(curr.data, end="")
                 break
             else:
-                print(current_node.data, end=" <-> ")
-                current_node = current_node.next
-        current_node = self.head
-        while current_node is not None:
-            if current_node.next is None:
-                print(current_node.data, end=" <-> ")
-                break
-            else:
-                print(current_node.data, end=" <-> ")
-                current_node = current_node.next
+                print(curr.data, end=" <-> ")
+                curr = curr.next
+        #current_node = self.head
+        #while current_node is not None:
+        #    if current_node.next is None:
+        #        print(current_node.data, end=" <-> ")
+        #        break
+        #    else:
+        #        print(current_node.data, end=" <-> ")
+        #        current_node = current_node.next
         pass
 
-    def sort2(self):
-        current_node = self.head
-        while current_node is not None:
-            if current_node.next is None:
-                print(current_node.data, end=" , ")
-                break
-            else:
-                print(current_node.data, end=" <-> ")
-                current_node = current_node.next
-        current_node = self.tail
-        while current_node is not None:
-            if current_node.prev is None:
-                print(current_node.data, end=" , ")
-                break
-            else:
-                print(current_node.data, end=" <-> ")
+    def sort2(self, deque):
+        #helper = 0
+        if deque.len_of() is not self.len_of():
+            curr1 = deque.head
+            curr2 = deque.tail
+            for i in range(0, self.len_of()):
+                curr2.data = curr1.data
+                curr1 = curr1.next
+                curr2 = curr2.prev
+        else:
+            current_node = self.tail
+            while current_node is not None:
+                deque.add(current_node.data)
                 current_node = current_node.prev
+        curr1 = deque.head
+        while curr1 is not None:
+            if curr1.next is None:
+                print(curr1.data, end="")
+                break
+            else:
+                print(curr1.data, end=" <-> ")
+                curr1 = curr1.next
+        # curr = deque.tail
+        # current_node = self.tail
+        # while current_node is not None:
+        #     if current_node.prev is None:
+        #         deque.add(current_node.data)
+        #         curr = curr.next
+        #         print(curr.data, end="")
+        #         #print(current_node.data, end=" , ")
+        #         break
+        #     else:
+        #         deque.add(current_node.data)
+        #         curr = curr.next
+        #         print(curr.data, end=" <-> ")
+        #     current_node = current_node.prev
         pass
 
-    def sort3(self):
-        current_node = self.tail
-        while current_node is not None:
-            if current_node.prev is None:
-                print(current_node.data, end=" , ")
+    def sort3(self, deque):
+        if deque.len_of() is self.len_of():
+            current = deque.head
+            current1 = self.head
+            current2 = self.tail
+            for i in range(0, self.len_of()):
+                current.data = current2.data
+                deque.add(current1.data)
+                current = current.next
+                current1 = current1.next
+                current2 = current2.prev
+        else:
+            current = self.tail
+            current1 = deque.head
+            current2 = deque.tail
+            for i in range(0, self.len_of()):
+                current1.data = current.data
+                current2.data = current1.data
+                current = current.prev
+                current1 = current1.next
+                current2 = current2.prev
+        current1 = deque.head
+        while current1 is not None:
+            if current1.next is None:
+                print(current1.data, end="")
                 break
             else:
-                print(current_node.data, end=" <-> ")
-                current_node = current_node.prev
-        current_node = self.head
-        while current_node is not None:
-            if current_node.next is None:
-                print(current_node.data, end=" , ")
-                break
-            else:
-                print(current_node.data, end=" <-> ")
-                current_node = current_node.next
+                print(current1.data, end=" <-> ")
+                current1 = current1.next
+        # current_node = self.head
+        # while current_node is not None:
+        #     if current_node.next is None:
+        #         print(current_node.data, end=" , ")
+        #         break
+        #     else:
+        #         print(current_node.data, end=" <-> ")
+        #         current_node = current_node.next
         pass
--- lab2/main.py
+++ lab2/main.py
@@ -4,7 +4,8 @@ from notebook.notebookapp import raw_input
 _author_ = 'Nikita Karatsev'
 _project_ = 'Lab2'
 
-d = DoubleLinkedList.DoubleLinkedList()
+d1 = DoubleLinkedList.DoubleLinkedList()
+d2 = DoubleLinkedList.DoubleLinkedList()
 ifs = ''
 '''a = {"0": d.fill_deque(),
          "1": d.sort1(),
@@ -13,17 +14,18 @@ ifs = ''
     }
 '''
 while ifs is not '~':
+    #"Welcome. 0 - add deque, 1-3 sort seque, ~ exit")
     print(
-        "\nДобро пожаловать в консоль. Что будем делать?!(0 - заполнить массив, 1..3 - сортировка и вывод на экран)")
+    "\nДобро пожаловать в консоль. Что будем делать?!(0 - заполнить массив, 1..3 - сортировка и вывод на экран)")
     ifs = raw_input()
     if ifs is '0':
-        d.fill_deque()
+        d1.fill_deque(d2)
     elif ifs is '1':
-        d.sort1()
+        d1.sort1(d2)
     elif ifs is '2':
-        d.sort2()
+        d1.sort2(d2)
     elif ifs is '3':
-        d.sort3()
+        d1.sort3(d2)
     elif ifs is '~':
         print("FBI OPEN DOOR!")
     else:
